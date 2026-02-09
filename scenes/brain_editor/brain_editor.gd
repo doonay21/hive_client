295868519b94c2f993859cbe1aa213d8d38065e3
doonay21 <dunajski.rafal@gmail.com>
2026-02-09 19:16:16 +0100

    WIP

--- scenes/brain_editor/brain_editor.gd
+++ scenes/brain_editor/brain_editor.gd
@@ -8,26 +8,91 @@ enum Size {
 }
 
 @export var size: Size = Size._5x5
+@export var edge_ports_active: bool = false
 
 @onready var grid_container: GridContainer = %GridContainer
 
 const slot_scene: PackedScene = preload("res://scenes/brain_editor/slot/slot.tscn")
+const edge_port_scene: PackedScene = preload("res://scenes/brain_editor/edge_port/edge_port.tscn")
+
+var context_menu: PopupMenu
+var current_selected_node: Control
 
 func _ready() -> void:
+	setup_context_menu()
 	initialize_grid()
 
+func setup_context_menu() -> void:
+	context_menu = PopupMenu.new()
+	add_child(context_menu)
+	
+	context_menu.add_separator("BE_EDGE_PORT_POPUP_MENU")
+	context_menu.add_item("BE_EDGE_PORT_NONE", 0)
+	context_menu.add_item("BE_EDGE_PORT_INPUT", 1)
+	context_menu.add_item("BE_EDGE_PORT_OUTPUT", 2)
+	
+	context_menu.id_pressed.connect(on_context_menu_id_pressed)
+
 func initialize_grid() -> void:
-	var columns: int = 5
+	var internal_dim: int = 5
 	
 	match size:
-		Size._3x3: columns = 3
-		Size._5x5: columns = 5
-		Size._7x7: columns = 7
-		Size._9x9: columns = 9
+		Size._3x3: internal_dim = 3
+		Size._5x5: internal_dim = 5
+		Size._7x7: internal_dim = 7
+		Size._9x9: internal_dim = 9
 
-	grid_container.columns = columns
+	var total_dim: int = internal_dim + 2
+	grid_container.columns = total_dim
 
-	for i in range(columns * columns):
-		var slot: Control = slot_scene.instantiate()
-		slot.name = "Slot%s" % i
-		grid_container.add_child(slot)
+	@warning_ignore("integer_division")
+	var center_index: int = total_dim / 2
+
+	for y in range(total_dim):
+		for x in range(total_dim):
+			var node: Control
+			var is_edge: bool = (x == 0 or x == total_dim - 1 or y == 0 or y == total_dim - 1)
+			
+			if is_edge:
+				var is_horizontal_center: bool = (x == center_index and (y == 0 or y == total_dim - 1))
+				var is_vertical_center: bool = (y == center_index and (x == 0 or x == total_dim - 1))
+				
+				if is_horizontal_center or is_vertical_center:
+					node = edge_port_scene.instantiate()
+					node.port_type = EdgePort.Port.NONE
+					
+					if y == 0:
+						node.direction = EdgePort.Direction.DOWN
+					elif y == total_dim - 1:
+						node.direction = EdgePort.Direction.UP
+					elif x == 0:
+						node.direction = EdgePort.Direction.RIGHT
+					elif x == total_dim - 1:
+						node.direction = EdgePort.Direction.LEFT
+				else:
+					node = Control.new()
+					node.custom_minimum_size = Vector2(84, 84)
+			else:
+				node = slot_scene.instantiate()
+			
+			node.name = "Node_%d_%d" % [x, y]
+			grid_container.add_child(node)
+			node.gui_input.connect(on_node_gui_input.bind(node))
+
+func on_node_gui_input(event: InputEvent, node: Control) -> void:
+	if event is InputEventMouseButton:
+		if (event.button_index == MOUSE_BUTTON_LEFT or event.button_index == MOUSE_BUTTON_RIGHT) and event.pressed:
+			current_selected_node = node
+			context_menu.position = Vector2(get_viewport().get_mouse_position())
+			context_menu.popup()
+
+func on_context_menu_id_pressed(id: int) -> void:
+	match id:
+		0:
+			current_selected_node.port_type = EdgePort.Port.NONE
+		1:
+			current_selected_node.port_type = EdgePort.Port.INPUT
+		2:
+			current_selected_node.port_type = EdgePort.Port.OUTPUT
+
+	current_selected_node.update_visuals()
---
+++ scenes/brain_editor/edge_port/edge_port.gd
@@ -0,0 +1,54 @@
+class_name EdgePort extends MarginContainer
+
+enum Port { NONE, INPUT, OUTPUT }
+enum Direction { UP, RIGHT, DOWN, LEFT }
+
+const TEX_NONE = preload("res://assets/images/brain_editor/conn_none.png")
+const TEX_IN = preload("res://assets/images/brain_editor/conn_in.png")
+const TEX_OUT = preload("res://assets/images/brain_editor/conn_out.png")
+
+@export var port_type: Port = Port.NONE
+@export var direction: Direction = Direction.UP
+
+@onready var block: TextureRect = $Control/Block
+@onready var port: TextureRect = $Control/Block/Port
+
+var hover_tween: Tween
+
+func _ready() -> void:
+	update_visuals()
+	
+	mouse_entered.connect(on_mouse_entered)
+	mouse_exited.connect(on_mouse_exited)
+
+func update_visuals() -> void:
+	if not is_node_ready(): return
+	
+	match port_type:
+		Port.NONE: port.texture = TEX_NONE
+		Port.INPUT: port.texture = TEX_OUT
+		Port.OUTPUT: port.texture = TEX_IN
+	
+	match direction:
+		Direction.UP: block.rotation_degrees = 0
+		Direction.RIGHT: block.rotation_degrees = 90
+		Direction.DOWN: block.rotation_degrees = 180
+		Direction.LEFT: block.rotation_degrees = 270
+
+func on_mouse_entered() -> void:
+	if hover_tween: hover_tween.kill()
+	
+	Events.info_text_requested.emit("BE_EDGE_PORT_INFO")
+	
+	hover_tween = create_tween()
+	hover_tween.set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
+	hover_tween.tween_property(self, "modulate", Color("ffffff80"), 0.2)
+
+func on_mouse_exited() -> void:
+	if hover_tween: hover_tween.kill()
+	
+	Events.info_text_hide_requested.emit()
+	
+	hover_tween = create_tween()
+	hover_tween.set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
+	hover_tween.tween_property(self, "modulate", Color("ffffff40"), 0.2)
